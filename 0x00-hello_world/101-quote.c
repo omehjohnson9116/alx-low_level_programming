0207a034c1f41a14eb3546dd8d98510505cc251b
Johnson Omeh <omehjohnson9116@gmail.com>
2022-10-14 00:12:16 -0700

    quote commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,8 @@
 
  * main - Entry point
 
+ *
+
  * Return: Always 1 (Success)
 
  */
@@ -12,13 +14,12 @@ int main(void)
 
 {
 
-        char str1[] = “and that piece of art is useful\” – Dora Korpar, 2015-10-19\n”;
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-        
 
-        write(2, str1, 59);
 
-        return (1);
+	write(2, str1, 59);
+
+	return (1);
 
 }
-
